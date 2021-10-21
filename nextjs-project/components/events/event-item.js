35fd427b829e0f0f8fbe4d75edc2f218b632bcda
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-21 18:12:22 +0200

    section 4-73

--- nextjs-project/components/events/event-item.js
+++ nextjs-project/components/events/event-item.js
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import classes from './event-item.module.css';
 
 function EventItem(props) {
   const { title, image, date, location, id } = props;
@@ -7,22 +8,22 @@ function EventItem(props) {
       month: 'long',
       year: 'numeric',
   });
-  const address = location.replace(',', '/n');
+  const address = location.replace(', ', '\n');
   const exploreLink = `/events/${id}`;
   return (
-    <li>
+    <li className={classes.item}>
       <img src={'/' + image} alt={title}/>
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{updatedDate}</time>
           </div>
           <div>
-            <address>{address}</address>
+            <address className={classes.address}>{address}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
