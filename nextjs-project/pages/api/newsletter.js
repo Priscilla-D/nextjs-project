b01a04292c7837f2d7c7822a4afe8dc3dc43412b
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-28 18:06:22 +0200

    section 9-155

--- nextjs-project/pages/api/newsletter.js
+++ nextjs-project/pages/api/newsletter.js
@@ -1,13 +1,23 @@
-function handler(req, res) {
-  if(req.method === 'POST') {
+import { MongoClient } from "mongodb";
+
+async function handler(req, res) {
+  if (req.method === "POST") {
     const userEmail = req.body.email;
 
-    if(!userEmail || !userEmail.includes('@')) {
-      res.status(422).json({ message: 'Invalid email address.'});
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({ message: "Invalid email address." });
       return;
     }
-    console.log(userEmail);
-    res.status(201).json({ message: 'Signed up!'});
+    const client = await MongoClient.connect(
+      'mongodb+srv://priscilla:placeshaker@cluster0.qujgr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+    );
+    console.log(client);
+    const db = client.db();
+    await db.collection('emails').insertOne({ email: userEmail });
+
+    client.close(); // déconnexion de la DB
+
+    res.status(201).json({ message: "Signed up!" });
   }
 }
 export default handler;
