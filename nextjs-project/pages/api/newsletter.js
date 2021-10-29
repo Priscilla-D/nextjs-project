0361cd1175b2bafa6b5a920eef90fd46afd4c15f
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-29 11:19:56 +0200

    section 9-159

--- nextjs-project/pages/api/newsletter.js
+++ nextjs-project/pages/api/newsletter.js
@@ -1,5 +1,17 @@
 import { MongoClient } from "mongodb";
 
+async function connectDatabase() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://priscilla:placeshaker@cluster0.qujgr.mongodb.net/events?retryWrites=true&w=majority"
+  );
+  return client;
+}
+
+async function insertDocument(client, document) {
+  const db = client.db();
+  await db.collection("newsletter").insertOne(document);
+}
+
 async function handler(req, res) {
   if (req.method === "POST") {
     const userEmail = req.body.email;
@@ -8,13 +20,23 @@ async function handler(req, res) {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
-    const client = await MongoClient.connect(
-      'mongodb+srv://priscilla:placeshaker@cluster0.qujgr.mongodb.net/events?retryWrites=true&w=majority'
-    );
-    const db = client.db();
-    await db.collection('newsletter').insertOne({ email: userEmail });
 
-    client.close(); // déconnexion de la DB
+    let client;
+    try {
+      client = await connectDatabase();
+    } catch (error) {
+      res.status(500).json({message: 'Connecting to the database failed.'});
+      return;
+    }
+
+    try {
+      await insertDocument(client, { email: userEmail });
+      client.close();
+    } catch (error) {
+      res.status(500).json({message: 'Inserting data failed.'});
+      return;
+    }
+
 
     res.status(201).json({ message: "Signed up!" });
   }
