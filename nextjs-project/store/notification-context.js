33db34acc373f5a050c4a7b5738e804d1d2c57df
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-29 15:28:24 +0200

    section 10-167

--- nextjs-project/store/notification-context.js
+++ nextjs-project/store/notification-context.js
@@ -1,14 +1,32 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 const NotificationContext = createContext({
   notification: null, // { title, message, status }
-  showNotification: function () {},
+  showNotification: function (notificationData) {},
   hideNotification: function () {},
 });
 
 export function NotificationContextProvider(props) {
+  const [activeNorification, setActiveNotification] = useState();
+  function showNotificationHandler(notificationData) {
+    setActiveNotification({
+      title: notificationData.title,
+      message: notificationData.message,
+      status: notificationData.status,
+    });
+  }
+  function hideNotificationHandler() {
+    setActiveNotification(null);
+  }
+
+  const context = {
+    notification: activeNotification,
+    showNotification: showNotificationHandler,
+    hideNotification: hideNotificationHandler,
+  };
+
   return (
-    <NotificationContext.Provider>
+    <NotificationContext.Provider value={context}>
       {props.children}
     </NotificationContext.Provider>
   );
