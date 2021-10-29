c432dcdd0be5aedf12de9780d950542837e5e69c
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-10-29 09:52:54 +0200

    section 9-157

--- nextjs-project/components/input/comment-list.js
+++ nextjs-project/components/input/comment-list.js
@@ -7,7 +7,7 @@ function CommentList(props) {
       {/* Render list of comments - fetched from API */}
       {items.map(item => 
       <div>
-      <li key={item.id}>
+      <li key={item._id}> {/* correction du message d'erreur "each child should have a unique key" */}
         <p>{item.text}</p>
         <div>
           By <address>{item.name}</address>
