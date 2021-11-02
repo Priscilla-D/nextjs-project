76e02363aa0496530c074bf35b444fe708ea5587
Priscilla-placeshaker <priscilla@placeshaker.fr>
2021-11-02 11:32:52 +0100

    section 10-171

--- nextjs-project/components/input/comment.js
+++ nextjs-project/components/input/comment.js
@@ -13,13 +13,16 @@ function Comments(props) {
 
   const [showComments, setShowComments] = useState(false);
   const [comments, setComments] = useState([]);
+  const [isFetchingComments, setIsFetchingComments] = useState(false);
 
   useEffect(() => {
     if (showComments) {
+      setIsFetchingComments(true);
       fetch("/api/comments/" + eventId)
         .then((response) => response.json())
         .then((data) => {
           setComments(data.comments);
+          setIsFetchingComments(false);
         });
     }
   }, [showComments]);
@@ -29,7 +32,11 @@ function Comments(props) {
   }
 
   function addCommentHandler(commentData) {
-    if (!commentData) {
+    notificationCtx.showNotification({
+          title: "Sending comment...",
+          message: "Your comment is currently being stored.",
+          status: "pending",
+        });
     fetch(`/api/comments/${eventId}`, {
       method: "POST",
       body: JSON.stringify(commentData),
@@ -37,7 +44,14 @@ function Comments(props) {
         "Content-type": "application/json",
       },
     })
-      .then((response) => response.json())
+      .then((response) => {
+        if (response.ok) {
+        return response.json();
+      }
+      return response.json().then((data) => {
+        throw new Error(data.message || "Something went wrong");
+      });
+    })
       .then((data) => {
         notificationCtx.showNotification({
           title: "Success...",
@@ -52,7 +66,6 @@ function Comments(props) {
           status: "error",
         });
       });
-    }
   }
 
   return (
@@ -61,7 +74,8 @@ function Comments(props) {
         {showComments ? "Hide" : "Show"} Comments
       </button>
       {showComments && <NewComment onAddComment={addCommentHandler} />}
-      {showComments && <CommentList items={comments}/>}
+      {showComments && !isFetchingComments && <CommentList items={comments}/>}
+      {showComments && isFetchingComments && <p>Loading...</p>}
     </section>
   );
 }
